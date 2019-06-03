9cfe65fc15a04ed3ffb9c1f9f3b422d64104efa2
Weyman Fung <wfung@pivotal.io>
2019-06-03 12:13:46 -0700

    add interesting question about merging arrays

---
+++ problems/combineTwoSortedArraysInPlace.js
@@ -0,0 +1,4 @@
+//Given an array
+//arr1 = [1, 1, 2, 5, 6, 0, 0, 0], arr2 = [4,7,8] n = 5, n is the elements in arr1 that are not zeros.
+//Merge arr2 into arr1. Arr1 will be modified in place while taking out the zeroes.
+
